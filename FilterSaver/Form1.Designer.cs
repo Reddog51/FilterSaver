28de67aae55681244d00514f3909ec6082364eb5
Reddog51 <robmeddy@gmail.com>
2018-06-12 12:48:19 -0700

    add controls.  controls will be iterated, type checked and value stored

--- FilterSaver/Form1.Designer.cs
+++ FilterSaver/Form1.Designer.cs
@@ -34,6 +34,18 @@
             this.btnAddFilter = new System.Windows.Forms.Button();
             this.tbAddFilter = new System.Windows.Forms.TextBox();
             this.btnDeleteFilter = new System.Windows.Forms.Button();
+            this.cboA = new System.Windows.Forms.ComboBox();
+            this.cboB = new System.Windows.Forms.ComboBox();
+            this.cboC = new System.Windows.Forms.ComboBox();
+            this.cboD = new System.Windows.Forms.ComboBox();
+            this.cboE = new System.Windows.Forms.ComboBox();
+            this.cboF = new System.Windows.Forms.ComboBox();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.lblD = new System.Windows.Forms.Label();
+            this.lblE = new System.Windows.Forms.Label();
+            this.lblF = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbSavedBongoFilters
@@ -93,11 +105,125 @@
             this.btnDeleteFilter.UseVisualStyleBackColor = true;
             this.btnDeleteFilter.Click += new System.EventHandler(this.btnDeleteFilter_Click);
             // 
+            // cboA
+            // 
+            this.cboA.FormattingEnabled = true;
+            this.cboA.Location = new System.Drawing.Point(66, 53);
+            this.cboA.Name = "cboA";
+            this.cboA.Size = new System.Drawing.Size(121, 21);
+            this.cboA.TabIndex = 6;
+            // 
+            // cboB
+            // 
+            this.cboB.FormattingEnabled = true;
+            this.cboB.Location = new System.Drawing.Point(66, 107);
+            this.cboB.Name = "cboB";
+            this.cboB.Size = new System.Drawing.Size(121, 21);
+            this.cboB.TabIndex = 7;
+            // 
+            // cboC
+            // 
+            this.cboC.FormattingEnabled = true;
+            this.cboC.Location = new System.Drawing.Point(66, 166);
+            this.cboC.Name = "cboC";
+            this.cboC.Size = new System.Drawing.Size(121, 21);
+            this.cboC.TabIndex = 8;
+            // 
+            // cboD
+            // 
+            this.cboD.FormattingEnabled = true;
+            this.cboD.Location = new System.Drawing.Point(235, 53);
+            this.cboD.Name = "cboD";
+            this.cboD.Size = new System.Drawing.Size(121, 21);
+            this.cboD.TabIndex = 9;
+            // 
+            // cboE
+            // 
+            this.cboE.FormattingEnabled = true;
+            this.cboE.Location = new System.Drawing.Point(235, 107);
+            this.cboE.Name = "cboE";
+            this.cboE.Size = new System.Drawing.Size(121, 21);
+            this.cboE.TabIndex = 10;
+            // 
+            // cboF
+            // 
+            this.cboF.FormattingEnabled = true;
+            this.cboF.Location = new System.Drawing.Point(235, 166);
+            this.cboF.Name = "cboF";
+            this.cboF.Size = new System.Drawing.Size(121, 21);
+            this.cboF.TabIndex = 11;
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(13, 53);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(14, 13);
+            this.lblA.TabIndex = 12;
+            this.lblA.Text = "A";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(13, 107);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(14, 13);
+            this.lblB.TabIndex = 13;
+            this.lblB.Text = "B";
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.Location = new System.Drawing.Point(12, 169);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(14, 13);
+            this.lblC.TabIndex = 14;
+            this.lblC.Text = "C";
+            // 
+            // lblD
+            // 
+            this.lblD.AutoSize = true;
+            this.lblD.Location = new System.Drawing.Point(382, 53);
+            this.lblD.Name = "lblD";
+            this.lblD.Size = new System.Drawing.Size(15, 13);
+            this.lblD.TabIndex = 15;
+            this.lblD.Text = "D";
+            // 
+            // lblE
+            // 
+            this.lblE.AutoSize = true;
+            this.lblE.Location = new System.Drawing.Point(382, 110);
+            this.lblE.Name = "lblE";
+            this.lblE.Size = new System.Drawing.Size(14, 13);
+            this.lblE.TabIndex = 16;
+            this.lblE.Text = "E";
+            // 
+            // lblF
+            // 
+            this.lblF.AutoSize = true;
+            this.lblF.Location = new System.Drawing.Point(382, 169);
+            this.lblF.Name = "lblF";
+            this.lblF.Size = new System.Drawing.Size(13, 13);
+            this.lblF.TabIndex = 17;
+            this.lblF.Text = "F";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblF);
+            this.Controls.Add(this.lblE);
+            this.Controls.Add(this.lblD);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.cboF);
+            this.Controls.Add(this.cboE);
+            this.Controls.Add(this.cboD);
+            this.Controls.Add(this.cboC);
+            this.Controls.Add(this.cboB);
+            this.Controls.Add(this.cboA);
             this.Controls.Add(this.btnDeleteFilter);
             this.Controls.Add(this.tbAddFilter);
             this.Controls.Add(this.btnAddFilter);
@@ -120,6 +246,18 @@
         private System.Windows.Forms.Button btnAddFilter;
         private System.Windows.Forms.TextBox tbAddFilter;
         private System.Windows.Forms.Button btnDeleteFilter;
+        private System.Windows.Forms.ComboBox cboA;
+        private System.Windows.Forms.ComboBox cboB;
+        private System.Windows.Forms.ComboBox cboC;
+        private System.Windows.Forms.ComboBox cboD;
+        private System.Windows.Forms.ComboBox cboE;
+        private System.Windows.Forms.ComboBox cboF;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.Label lblD;
+        private System.Windows.Forms.Label lblE;
+        private System.Windows.Forms.Label lblF;
     }
 }
 
